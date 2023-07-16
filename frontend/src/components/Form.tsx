d05a78d895ab5acec629a2ece71bebed8049cc35
Andriy Zagoruyko <andriy@DESKTOP-5NTS6MC.localdomain>
2023-07-16 14:33:24 +0300

    feat: improve validation

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -4,6 +4,7 @@ import {
   IconButton,
   Paper,
   TextField,
+  Tooltip,
   Typography,
 } from "@material-ui/core";
 import Autocomplete from "@mui/material/Autocomplete";
@@ -16,58 +17,82 @@ import Stack from "@mui/material/Stack";
 const URL_REGEX =
   /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
 
-const EMPTY_ITEM = { fileLink: "", lang: "", isValid: true };
+const EMPTY_ROW = {
+  fileLink: "",
+  lang: "",
+  isLinkValid: false,
+  isLangValid: false,
+  isTouched: false,
+};
+
+interface IRow {
+  fileLink: string;
+  lang: string;
+  isLinkValid: boolean;
+  isLangValid: boolean;
+  isTouched: boolean;
+}
 
 export function Form() {
   const styles = useStyles();
-  const [items, setItems] = useState([EMPTY_ITEM]);
+  const [rows, setRows] = useState<IRow[]>([EMPTY_ROW]);
 
-  const handleAddItem = () => setItems([...items, EMPTY_ITEM]);
-
-  const handleLinkChange = (index: number, fileLink: string) => {
-    const updatedItems = [...items];
-    updatedItems[index] = {
-      ...updatedItems[index],
-      fileLink,
-      isValid: validateLink(fileLink),
-    };
-    setItems(updatedItems);
+  const updateRow = (index: number, updatedItem: IRow) => {
+    const nextItems = [...rows];
+    nextItems[index] = updatedItem;
+    setRows(nextItems);
+    validateRow(index, nextItems[index]);
   };
 
-  const handleLangChange = (index: number, lang: string) => {
-    const updatedItems = [...items];
-    updatedItems[index] = { ...updatedItems[index], lang };
-    setItems(updatedItems);
-  };
+  const handleAddRow = () => setRows([...rows, EMPTY_ROW]);
 
-  const handleDeleteItem = (index: number) => {
-    if (items.length > 1) {
-      setItems(items.filter((_, i) => i !== index));
+  const handleLinkChange = (index: number, fileLink: string) =>
+    updateRow(index, { ...rows[index], fileLink });
+
+  const handleLangChange = (index: number, lang: string) =>
+    updateRow(index, { ...rows[index], lang });
+
+  const handleDeleteRow = (index: number) => {
+    if (rows.length > 1) {
+      setRows(rows.filter((_, i) => i !== index));
     }
   };
 
-  const validateLink = (link: string) => {
-    return !!link.match(URL_REGEX);
+  const validateRow = (index: number, item: IRow) => {
+    const nextItems = [...rows];
+    nextItems[index] = {
+      ...item,
+      isLinkValid: !!item.fileLink.match(URL_REGEX),
+      isLangValid: !!item.lang,
+      isTouched: true,
+    };
+    setRows(nextItems);
   };
+
+  const hasValidRows = rows.every((row) => row.isLinkValid && row.isLangValid);
 
   return (
     <>
       <Box className={styles.formContainer}>
         <Typography variant="h4">Past your link</Typography>
-        {items.map((item, index) => (
+        {rows.map((row, index) => (
           <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
             <IconButton
-              onClick={() => handleDeleteItem(index)}
-              disabled={items.length <= 1}
+              onClick={() => handleDeleteRow(index)}
+              disabled={rows.length <= 1}
             >
               <DeleteIcon />
             </IconButton>
             <Paper className={styles.formRow}>
               <TextField
-                error={!item.isValid}
+                error={!row.isLinkValid && row.isTouched}
                 className={styles.field}
-                label={!item.isValid ? "Link is not valid" : "File link"}
-                value={item.fileLink}
+                label={
+                  !row.isLinkValid && row.isTouched
+                    ? "Link is not valid"
+                    : "File link"
+                }
+                value={row.fileLink}
                 onChange={({ target }) => handleLinkChange(index, target.value)}
               />
               <Autocomplete
@@ -76,25 +101,35 @@ export function Form() {
                 options={["UKR", "EN"]}
                 sx={{ minWidth: 100 }}
                 renderInput={(params) => (
-                  <TextField {...params} label="Language" />
+                  <TextField
+                    {...params}
+                    error={!row.isLangValid && row.isTouched}
+                    label="Language"
+                  />
                 )}
               />
             </Paper>
           </Stack>
         ))}
         <Stack direction="row" spacing={2} className={styles.buttonsContainer}>
-          <Button variant="contained" onClick={handleAddItem}>
+          <Button variant="contained" onClick={handleAddRow}>
             Add more
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            disabled={
-              !items.some(({ fileLink, lang, isValid }) => isValid && !!lang)
-            }
-          >
-            Send
-          </Button>
+          {hasValidRows ? (
+            <Button variant="contained" color="primary">
+              Send
+            </Button>
+          ) : (
+            <Tooltip title="All fields should be specified">
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={!hasValidRows}
+              >
+                Send
+              </Button>
+            </Tooltip>
+          )}
         </Stack>
       </Box>
     </>
